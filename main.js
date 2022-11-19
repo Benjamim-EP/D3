c691d1cf4baa0d4383e308bb973a77eed0cd9a35
Benjamim-EP <bannk18@gmail.com>
2022-11-19 13:20:37 -0300

    Gráfico de barras com modulos

---
+++ main.js
@@ -0,0 +1,20 @@
+import bar from './bar1.js';
+
+
+let container = d3.select('.chart-container');
+let barChart = bar();
+let dataset = await d3.csv("data.csv")
+
+barChart
+    .width(300)
+    .height(200)
+    .margin({
+            left: 50,
+            bottom: 30
+            })
+            .on('customMouseOver', function(event, data) {
+                console.log('data', data);
+            });
+            
+console.log
+container.datum(dataset).call(barChart);
